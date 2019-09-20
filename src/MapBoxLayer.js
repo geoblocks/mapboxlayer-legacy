54d706b9049c28475ba3a1d45b5adb87e30a7ac2
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-09-20 14:11:59 +0200

    Load the map lazily

--- src/MapBoxLayer.js
+++ src/MapBoxLayer.js
@@ -27,54 +27,56 @@ export class MapBoxLayerRenderer extends Observable {
 
   /**
    * Create a layer renderer.
-   * @param {import("../Map.js").default} mapRenderer The map renderer.
+   * @param {import("../Map.js").default} _ The map renderer.
    * @param {import("../../layer/Layer.js").default} layer The layer to be rendererd.
-   * @return {CanvasImageLayerRenderer} The layer renderer.
+   * @return {MapBoxLayerRenderer} The layer renderer.
    */
-  static create(mapRenderer, layer) {
+  static create(_, layer) {
     return new MapBoxLayerRenderer(/** @type {MapBoxLayer} */ (layer));
   }
 
   /**
+   * Called by the OpenLayer renderer on render if the layer is visible.
    * @param {import('ol/PluggableMap.js').FrameState} frameState
    */
   prepareFrame(frameState) {
     const layer = this.layer;
-    const mapboxMap = this.layer.mapboxMap;
-    const canvas = mapboxMap.getCanvas();
-    const viewState = frameState.viewState;
 
-    const visible = layer.getVisible();
-    canvas.style.display = visible ? 'block' : 'none';
+    // Eventually initialze the MapBox map.
+    const map = layer.getMapBoxMap();
 
+    const canvas = map.getCanvas();
     const opacity = layer.getOpacity().toString();
     if (opacity !== canvas.style.opacity) {
       canvas.style.opacity = opacity;
     }
 
-    // adjust view parameters in mapbox
-    const rotation = viewState.rotation;
-    if (rotation) {
-      mapboxMap.rotateTo(toDegrees(-rotation), {
-        animate: false
-      });
-    }
-    mapboxMap.jumpTo({
+    // Adjust view parameters in mapbox
+    const viewState = frameState.viewState;
+    const rotation = viewState.rotation || 0;
+    map.jumpTo({
+      bearing: toDegrees(-rotation),
       center: toLonLat(viewState.center),
       zoom: viewState.zoom - 1,
       animate: false
     });
 
-    // cancel the scheduled update & trigger synchronous redraw
-    // see https://github.com/mapbox/mapbox-gl-js/issues/7893#issue-408992184
-    // NOTE: THIS MIGHT BREAK WHEN UPDATING MAPBOX
-    if (mapboxMap._frame) {
-      mapboxMap._frame.cancel();
-      mapboxMap._frame = null;
-    }
-    mapboxMap._render();
+    this.triggerSynchronousMapboxRendering_(map);
+  }
 
-    return false; // never call compose
+  /**
+   * Cancel the scheduled update and trigger a synchronous redraw
+   * using some private APIs: this MIGHT BREAK in a future MapBox version!
+   * See https://github.com/mapbox/mapbox-gl-js/issues/7893#issue-408992184
+   * @param {mapboxgl.Map} map
+   */
+  triggerSynchronousMapboxRendering_(map) {
+
+    if (map._frame) {
+      map._frame.cancel();
+      map._frame = null;
+    }
+    map._render();
   }
 }
 
@@ -93,20 +95,52 @@ export default class MapBoxLayer extends Layer {
       mapboxgl.accessToken = options.accessToken;
     }
 
-    this.mapboxMap = new mapboxgl.Map({
-      container: options.container,
-      style: options.style,
-      attributionControl: false,
-      boxZoom: false,
-      doubleClickZoom: false,
-      dragPan: false,
-      dragRotate: false,
-      interactive: false,
-      keyboard: false,
-      pitchWithRotate: false,
-      scrollZoom: false,
-      touchZoomRotate: false
+    this.container_ = options.container;
+    this.style_ = options.style;
+
+    /**
+     * @type {mapboxgl.Map}
+     */
+    this.mapboxMap = null;
+
+    // this.getMapBoxMap();
+    this.on('change:visible', evt => {
+      this.updateVisibility_();
     });
+  }
+
+  /**
+   * @return {!mapboxgl.Map} The lazily initialized map
+   */
+  getMapBoxMap() {
+    if (!this.mapboxMap) {
+      this.mapboxMap = new mapboxgl.Map({
+        container: this.container_,
+        style: this.style_,
+        attributionControl: false,
+        boxZoom: false,
+        doubleClickZoom: false,
+        dragPan: false,
+        dragRotate: false,
+        interactive: false,
+        keyboard: false,
+        pitchWithRotate: false,
+        scrollZoom: false,
+        touchZoomRotate: false
+      });
+      this.updateVisibility_();
+    }
+    return this.mapboxMap;
+  }
+
+  updateVisibility_() {
+    const map = this.getMapBoxMap();
+    const visible = this.getVisible();
+    const visibility = visible ? 'block' : 'none';
+    const canvas = map.getCanvas();
+    if (canvas.style.display !== visibility) {
+      canvas.style.display = visibility;
+    }
   }
 
   getSourceState() {
@@ -114,10 +148,11 @@ export default class MapBoxLayer extends Layer {
   }
 
   /**
-   * @param {string} name
+   * Change visibility of a sublayer.
+   * @param {string} layername
    * @param {boolean} visible
    */
-  setLayerVisibility(name, visible) {
-    this.mapboxMap.setLayoutProperty(name, 'visibility', visible ? 'visible' : 'none');
+  setLayerVisibility(layername, visible) {
+    this.mapboxMap.setLayoutProperty(layername, 'visibility', visible ? 'visible' : 'none');
   }
 }
